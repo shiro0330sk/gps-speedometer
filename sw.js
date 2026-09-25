2e3c042919664f96cbc5b52df262424f2569d862
shiro0330sk <shiro0330sk@users.noreply.github.com>
2026-09-25 20:49:28 +0900

    Add CSV-based lap target settings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gps-speedometer-v2';
+const CACHE='gps-speedometer-v3';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
